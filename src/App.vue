104b9227f112aba72d765227e747abe70d7aca61
tahademirer <s.tahademirer@gmail.com>
2022-07-26 14:41:56 +0300

    Updates.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TheAnimation></TheAnimation>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheAnimation from "./components/TheAnimation.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TheAnimation,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/TheAnimation.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="wrapper" id="ahab"></div>
+  </div>
+</template>
+
+<script>
+import { init } from "animation-sdk/animations/confetti/confetti";
+import "animation-sdk/animations/confetti/styles.css";
+import { onMounted } from "@vue/runtime-core";
+export default {
+  setup() {
+    onMounted(async () => {
+      await init("ahab");
+    });
+  },
+};
+</script>
+
+<style></style>
